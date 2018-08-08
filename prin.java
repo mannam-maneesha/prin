05344f99fa888f588001e61e948403ef02ead0cc
mannam-maneesha <41893993+mannam-maneesha@users.noreply.github.com>
2018-08-08 20:38:22 +0530

    Create prin.java

---
+++ prin.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int a,b;
+     Scanner sc=new Scanner(System.in);
+    a=sc.nextInt();
+    b=sc.nextInt();
+    	for (a=a+1; a < b; a++)
+   {
+			
+    if (a % 2 != 0)
+    {
+     System.out.print(a+" ");
+     }
+    }	// your code goes here
+	}
+}
